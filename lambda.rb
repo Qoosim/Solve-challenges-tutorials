1d5516caf81d86594755e6a6e46ce5faf98b1c0f
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-09-10 14:04:25 +0100

    Practise some challenges from hackerrank and rubyMonk

--- lambda.rb
+++ lambda.rb
@@ -21,3 +21,29 @@ area_triangle = 0.5 * area.call(x, y)
 
 puts area_rectangle  # 200.0
 puts area_triangle   # 100.0
+puts "======================"
+
+# Hackerrank challenge
+square = -> (a) { a**2 }
+p square.(2)
+plus_one = -> (a) { a.next }
+p plus_one.call(3)
+adder = -> (a, b) { a + b }
+p adder.call(2, 2)
+values_only = -> (a) { a.values }
+p values_only.call(a: 1, b: 2)
+
+## Still lessons under lambda
+puts "======================"
+
+multiply_function = -> (number) do
+  -> (another_number) do
+      number * another_number
+  end
+end
+
+doubler = multiply_function.(3)
+tripple = multiply_function.(5)
+
+puts doubler.(10)
+puts tripple.(15)
---
+++ ruby_monk_challenge.rb
@@ -0,0 +1,7 @@
+def find_frequency(sentence, word)
+  splitted_word = sentence.downcase.split(' ')
+  return splitted_word.count(word.downcase)
+end
+
+sentence = "Ruby is the best language in the world"
+p find_frequency(sentence, 'the')
